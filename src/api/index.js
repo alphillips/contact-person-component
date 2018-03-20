bc1e59cf542d44c176ccf9456251809b12535109
[zapjelly] <[pak.cindy@gmail.com]>
2018-03-20 12:02:38 +1100

    update

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,5 @@ const URL_BASE = (process.env.API_HOST || '') + '/api/'
 import {get, post, put, del, formPost} from '@react-ag-components/core/lib/api'
 
 export function findClientEmailID(type, id) {
-       return get('/nexdoc/api/v1/contactperson/' + type + "/id/" + id)
+       return get(URL_BASE + 'v1/contactperson/' + type + "/id/" + id)
 }
